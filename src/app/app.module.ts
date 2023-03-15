58e2f142c52f9c5c54a11adbdcd9f320aaaa4740
Krishna Kant <tripathikrishnakanttripathi@gmail.com>
2023-03-16 00:17:33 +0530

    front page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerAreaComponent } from './banner-area/banner-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerAreaComponent
   ],
   imports: [
     BrowserModule,
